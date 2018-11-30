57c917f53a47491e4850c85aa53ff7caea508a7e
Donovan Triplett <donotriplett@gmail.com>
2018-11-30 10:30:01 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AuthComponent,
+    ProfileComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
