5e1606a1e9b3ccb281348cc08b0a408519b53308
Nic Temple <nictemple@Nics-MacBook-Pro.local>
2018-12-04 13:56:56 -0500

    landing_page_working_links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { LandingComponent } from './landing/landing.component'
+import { PostsComponent } from './posts/posts.component';
+import { AuthComponent } from './auth/auth.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo:'/landing', pathMatch: 'full'},
+  {path: 'landing', component: LandingComponent },
+  {path: 'posts', component: PostsComponent },
+  {path: 'account', component: AuthComponent },
+
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
