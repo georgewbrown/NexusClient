987d74c6ac4ced2b23cf67fa54747d256a770651
George <george@Georges-MacBook-Pro.local>
2018-12-11 09:21:28 -0500

    added CRUD to post

---
+++ src/app/models/post.ts
@@ -0,0 +1,8 @@
+export class Post {
+    id: number;
+    jobDescription: string;
+    jobTitle: "Job1"
+    location: "Awkuje,Finland"
+    payRange: "$16864.18"
+    skills: "Auditing"
+}
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,16 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "application/json",
+    "Authorization": localStorage.getItem("token")
+  })
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class PostsService {
-
+  private _URL = "https://nexuserver.herokuapp.com/post"
   constructor(private http: HttpClient) { }
 
-  getPosts(): Observable<any> {
-    return this.http.get<any>("https://nexuserver.herokuapp.com/post/all")
+
+
+  getPosts(): Observable<any[]> {
+    return this.http.get<any[]>(`${this._URL}/all`)  }
+
+  /** POST: add a new hero to the server */
+  createPost(post: object) : Observable<any[]>{
+    return this.http.post<any[]>(`${this._URL}/create`, post, httpOptions)
+  }  
+  
+  /** PUT: update the hero on the server */
+  updatePost(post: object): Observable<any> {
+    return this.http.put<any[]>(`${this._URL}/update/${post}`, httpOptions)
+  }  
+  
+  deletePost(id: number) {
+    return this.http.delete(`${this._URL}/delete/${id}`, httpOptions)
   }
+    
 }
