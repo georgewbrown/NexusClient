6c42bca4940ca5b1973dc9102439eda5f014a9e6
George <george@Georges-MacBook-Pro.local>
2018-12-11 09:23:38 -0500

    fixed post model

--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,8 +1,8 @@
 export class Post {
     id: number;
     jobDescription: string;
-    jobTitle: "Job1"
-    location: "Awkuje,Finland"
-    payRange: "$16864.18"
-    skills: "Auditing"
+    jobTitle: string;
+    location: string;
+    payRange: string;
+    skills: string;
 }
