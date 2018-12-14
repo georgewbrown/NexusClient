c8849a7f6cf4775606a8ec9b98e837c498d22758
Donovan Triplett <donotriplett@gmail.com>
2018-12-14 10:53:09 -0500

    12/14/18

--- src/app/models/employer.model.ts
+++ src/app/models/employer.model.ts
@@ -1,14 +1,16 @@
 export class Employer {
-    id: string;
-    username: string;
-    name: string;
-    password: string;
-    email: string;
-    phoneNumber: number;
-    profilePicture: string;
-    location: string;
-    website: string;
-    about: string;
+    business: {
+        id: string;
+        username: string;
+        name: string;
+        password: string;
+        email: string;
+        phoneNumber: number;
+        profilePicture: string;
+        location: string;
+        website: string;
+        about: string;
+    }
     posts
     sessionToken: string
 }
