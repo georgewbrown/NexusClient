bb377418fe5c3ca4997bb5f1c48fd4d026489724
George <george@Georges-MacBook-Pro.local>
2018-12-17 13:36:06 -0500

    changed admin

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,43 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { AdminService } from '../services/admin.service';
+import { Component, OnInit,HostBinding } from '@angular/core';
+import { AdminService } from '../services/admin.service'
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
-  styleUrls: ['./admin-login.component.css']
+  styleUrls: ['./admin-login.component.css'],
 })
 export class AdminLoginComponent implements OnInit {
-  
-  adminForm: FormGroup
-  
-    //Form state
-    loading = false;
-    success = false;
-  
-  constructor(private fb: FormBuilder, private as: AdminService) { }
 
-  ngOnInit() {
-    this.adminForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+error: any;
+  constructor(public as: AdminService, private router: Router) {
+
+    this.as.auth.subscribe( auth => {
+      if(auth) {
+        this.router.navigateByUrl('/landing');
+      }
     });
+   }
+
+  ngOnInit(){
+
   }
-  async submitHandler() {
-    this.loading = true;
-
-    const formValue = this.adminForm.value;
-
-    // try{
-    //   await this.as.collection('contacts').add(formValue);
-    //   this.success = true;
-    // }catch(err) {
-    //   console.error(err)
-    // }
-
-    this.loading = false
-  }
-
 
 }
-
