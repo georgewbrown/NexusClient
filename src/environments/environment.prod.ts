9a47dabd60cc1f6d375ab573cc7ac3154eb51c2a
Nic Temple <nictemple@Nics-MacBook-Pro.local>
2018-12-18 13:33:29 -0500

    deployed clients side of nexus

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
-export const environment = {
-  production: true
-};
+export let APIURL = '';
+
+switch (window.location.hostname) {
+  case 'https://nexus-client.herokuapp.com/' :
+    APIURL = 'https://nexuserver.herokuapp.com/'
+    break;
+  default:
+    APIURL = 'http://localhost:4200'
+}
+
